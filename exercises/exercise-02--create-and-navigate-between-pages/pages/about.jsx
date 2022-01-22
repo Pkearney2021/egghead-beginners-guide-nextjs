580dd3a238a3535ee030e7bdbd74dbef05e7c34d
Phil Kearney <philip.kearney@personio.de>
2022-01-22 19:44:40 +0000

    Added in some pages and the ability to navigate between the pages using the next/link component

---
+++ exercises/exercise-02--create-and-navigate-between-pages/pages/about.jsx
@@ -0,0 +1,12 @@
+import Link from 'next/link'
+
+const About = () => {
+  return (
+    <div>
+      <h2>Hello from about page!!</h2>
+      <Link href="/">Go back to home page</Link>
+    </div>
+  )
+}
+
+export default About
